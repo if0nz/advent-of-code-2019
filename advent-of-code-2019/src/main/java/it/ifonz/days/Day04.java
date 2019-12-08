3c5f89c4d1bd86309d73b86462e2dee538f9e765
ifonz <ifonz@ifonz-GL553VD>
2019-12-08 14:39:42 +0100

    optmized d04

--- advent-of-code-2019/src/main/java/it/ifonz/days/Day04.java
+++ advent-of-code-2019/src/main/java/it/ifonz/days/Day04.java
@@ -8,7 +8,7 @@ import it.ifonz.common.FileReader;
 public class Day04 {
 
 	public static void main(String args[]) throws URISyntaxException, IOException {
-		var range = FileReader.readLine("/d04.txt").split("-");
+		var range = FileReader.readLines("/d04.txt").get(0).split("-");
 		int[] numericRange = { Integer.parseInt(range[0]), Integer.parseInt(range[1]) };
 		part1(numericRange);
 		part2(numericRange);
@@ -16,19 +16,24 @@ public class Day04 {
 
 	public static void part1(int[] range) {
 		int valid = 0;
+		boolean sameDigit;
+		int d1,d2;
+		int n;
 		for (int i = range[0]; i<=range[1]; i++) {
-			boolean sameDigit = false;
-			int n = i;
-			int d1,d2;
+			n = i;
+			sameDigit = false;
 			do  {
 				d1 = n % 10;
 				n /= 10;
 				d2 = n % 10;
-				if (d1 < d2)
+				if (d1 < d2) {
+					sameDigit = false;
+					n=0;
 					break;
+				}
 				sameDigit = sameDigit || (d1 == d2);
 			}while (n != 0);
-			valid+= d1>=d2 && sameDigit ?1:0;
+			valid+= sameDigit ?1:0;
 		}
 		System.out.println(valid);
 	}
@@ -36,17 +41,23 @@ public class Day04 {
 	public static void part2(int[] range) {
 		
 		int valid = 0;
+		int n;
+		int sameDigitNumber = 0;
+		boolean sameDigitPair = false;
+		int d1,d2;
 		for (int i = range[0]; i<=range[1]; i++) {
-			int n = i;
-			int sameDigitNumber = 0;
-			boolean sameDigitPair = false;
-			int d1,d2;
+			n = i;
+			sameDigitNumber = 0;
+			sameDigitPair = false;
 			do {
 				d1 = n % 10;
 				n /= 10;
 				d2 = n % 10;
-				if (d1 < d2)
+				if (d1 < d2){
+					n=0;
+					sameDigitPair=false;
 					break;
+				}
 				if (d1 == d2) {
 					sameDigitNumber = sameDigitNumber * 10 + d1;
 				} else if (sameDigitNumber != 0) {
@@ -54,7 +65,7 @@ public class Day04 {
 					sameDigitNumber = 0;
 				}
 			} while (n != 0);
-			valid += d1>=d2 && sameDigitPair ? 1 : 0;
+			valid += sameDigitPair ? 1 : 0;
 		}
 		System.out.println(valid);
 	}
