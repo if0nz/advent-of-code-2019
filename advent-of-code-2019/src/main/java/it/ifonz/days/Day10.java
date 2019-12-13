369d2e26d2e3a0f585660151e59c2f6ccc7c10d1
user <user@SL_1724>
2019-12-13 18:33:55 +0100

    :shrug:

--- advent-of-code-2019/src/main/java/it/ifonz/days/Day10.java
+++ advent-of-code-2019/src/main/java/it/ifonz/days/Day10.java
@@ -29,24 +29,23 @@ public class Day10 {
 				var station = new Coord(station_x, station_y);
 				ArrayList<Coord> asteroidsDetected = new ArrayList<>();
 				asteroids.put(station, asteroidsDetected);
-				if ('#' == grid[station_x][station_y]) {
+				if ('#' == grid[station_y][station_x]) {
 					for (int asteroid_x = 0; asteroid_x < grid.length; asteroid_x++) {
 						for (int asteroid_y = 0; asteroid_y < grid.length; asteroid_y++) {
 							// same coords
-							if (Math.abs(asteroid_y - station_y) + Math.abs(asteroid_x - station_x) == 0) {
-							}
+							if (Math.abs(asteroid_y - station_y) + Math.abs(asteroid_x - station_x) == 0)
+								continue;
 							// no asteoroid in the testing coods
-							else if ('#' != grid[asteroid_x][asteroid_y]) {
-							}
+							if ('#' != grid[asteroid_y][asteroid_x])
+								continue;
 							// 1st asteroid ever seen
-							else if (asteroidsDetected.isEmpty()) {
+							if (asteroidsDetected.isEmpty()) {
 								asteroidsDetected.add(new Coord(asteroid_x, asteroid_y));
 							}
 							// check if this asteroid is obsured by other asteroids already seen
-							else if (test(asteroidsDetected, asteroid_x, asteroid_y, station)) {
+							else if (test(asteroidsDetected, asteroid_x, asteroid_y, station) == null) {
 								asteroidsDetected.add(new Coord(asteroid_x, asteroid_y));
 							}
-
 						}
 					}
 				}
@@ -63,30 +62,35 @@ public class Day10 {
 				var station = new Coord(station_x, station_y);
 				ArrayList<Coord> asteroidsDetected = new ArrayList<>();
 				asteroids.put(station, asteroidsDetected);
-				if ('#' == grid[station_x][station_y]) {
+				if ('#' == grid[station_y][station_x]) {
 					for (int asteroid_x = 0; asteroid_x < grid.length; asteroid_x++) {
 						for (int asteroid_y = 0; asteroid_y < grid.length; asteroid_y++) {
 							// same coords
 							var currentAsteroid = new Coord(asteroid_x, asteroid_y);
+							
 							if (station.distance(currentAsteroid) == 0) {
 							}
 							// no asteoroid in the testing coods
-							else if ('#' != grid[asteroid_x][asteroid_y]) {
+							else if ('#' != grid[asteroid_y][asteroid_x]) {
 							}
 							// 1st asteroid ever seen
 							else if (asteroidsDetected.isEmpty()) {
 								asteroidsDetected.add(new Coord(asteroid_x, asteroid_y));
 							}
 							// check if this asteroid is obsured by other asteroids already seen
-							else if (test(asteroidsDetected, asteroid_x, asteroid_y, station)) {
-
-								var newAsteroidDetected = new Coord(asteroid_x, asteroid_y);
-								if (station.distance(newAsteroidDetected) < station.distance(currentAsteroid)) {
-									asteroidsDetected.remove(currentAsteroid);
+							else {
+								// if it's not insert it into the list
+								Coord existingAsteroid = test(asteroidsDetected, asteroid_x, asteroid_y, station);
+								if (existingAsteroid == null) 
+									asteroidsDetected.add(currentAsteroid);
+								// else test its distance with the one laying in the same line. if the asteroid is nearer to the station than the existing one, then replace the latter with the former
+								else {
+									if (currentAsteroid.distance(station) < existingAsteroid.distance(station)) {
+										asteroidsDetected.remove(existingAsteroid);
+										asteroidsDetected.add(currentAsteroid);
+									}
 								}
-								asteroidsDetected.add(newAsteroidDetected);
 							}
-
 						}
 					}
 				}
@@ -96,64 +100,36 @@ public class Day10 {
 		var stationCoords = station.getKey();
 		var destroyingAsteroids = station.getValue();
 		destroyingAsteroids.sort((c1, c2) -> {
-			var angle1 = Math.atan2(c1.x-stationCoords.x,c1.y-stationCoords.y);
-			var angle2 = Math.atan2(c2.x-stationCoords.x,c2.y-stationCoords.y);
-			return Double.compare(angle1,angle2);
+			return extracted(stationCoords, c1, c2);
 		});
-		int destroyedAsteroids = 0;
-		Coord asteroid200 = null;
-		do {
-			for (int i = 0; i < destroyingAsteroids.size() && destroyedAsteroids < 200; i++) {
-				asteroid200 = destroyingAsteroids.get(i);
-				grid[asteroid200.x][asteroid200.y] = '.';
-				destroyedAsteroids++;
-			}
-			if (destroyedAsteroids < 200) {
-				ArrayList<Coord> asteroidsDetected = new ArrayList<>();
-				if ('#' == grid[stationCoords.x][stationCoords.y]) {
-					for (int asteroid_x = 0; asteroid_x < grid.length; asteroid_x++) {
-						for (int asteroid_y = 0; asteroid_y < grid.length; asteroid_y++) {
-							// same coords
-							var currentAsteroid = new Coord(asteroid_x, asteroid_y);
-							if (stationCoords.distance(currentAsteroid) == 0) {
-							}
-							// no asteoroid in the testing coods
-							else if ('#' != grid[asteroid_x][asteroid_y]) {
-							}
-							// 1st asteroid ever seen
-							else if (asteroidsDetected.isEmpty()) {
-								asteroidsDetected.add(new Coord(asteroid_x, asteroid_y));
-							}
-							// check if this asteroid is obsured by other asteroids already seen
-							else if (test(asteroidsDetected, asteroid_x, asteroid_y, stationCoords)) {
 
-								var newAsteroidDetected = new Coord(asteroid_x, asteroid_y);
-								if (stationCoords.distance(newAsteroidDetected) < stationCoords
-										.distance(currentAsteroid)) {
-									asteroidsDetected.remove(currentAsteroid);
-									asteroidsDetected.add(newAsteroidDetected);
-								}
-							}
-
-						}
-					}
-				}
-			}
-		} while (destroyedAsteroids < 200);
+		Coord asteroid200 = destroyingAsteroids.get(200);
+//		for (Coord c : destroyingAsteroids) {
+//			if (c.equals(a1)) break;
+//			System.out.println(extracted(stationCoords, a1, c));
+//		}
+		System.out.println(extracted(stationCoords, new Coord(10,16), new Coord(10,9)));
 		System.out.println(asteroid200.x * 100 + asteroid200.y);
 
 	}
 
+	private static int extracted(Coord stationCoords, Coord c1, Coord c2) {
+		var angle1 = Math.atan2(c1.y - stationCoords.y, c1.x - stationCoords.x);
+		var angle2 = Math.atan2(c2.y - stationCoords.y, c2.x - stationCoords.x);
+		angle1 = angle1 < -Math.PI/2 ? angle1 + Math.PI : angle1;
+		angle2 = angle2 < -Math.PI/2 ? angle2 + Math.PI : angle2;
+		return Double.compare(angle1, angle2);
+	}
+
 	// 0,4 2,0
-	private static boolean test(ArrayList<Coord> asteroidsDetected, int asteroid_x, int asteroid_y, Coord station) {
+	private static Coord test(ArrayList<Coord> asteroidsDetected, int asteroid_x, int asteroid_y, Coord station) {
 		var c = asteroidsDetected.stream().filter(asteroid -> {
 			return testSingle(asteroid, station, asteroid_x, asteroid_y);
-		}).count();
-		return c == 0;
+		}).findFirst().orElse(null);
+		return c;
 	}
 
 	private static boolean testSingle(Coord asteroid, Coord station, int asteroid_x, int asteroid_y) {
-//		boolean sinNot0 = asteroid.y != station.y || asteroid_y != station.y;
 		/*
 		 * if 2 points on a plane are aligned with an origin point, then they have the
 		 * same angle with the origin. to test this, one can check if tan(origin,a) ==
@@ -161,8 +137,8 @@ public class Day10 {
 		 * properties to transorm equality between fractions in equality between
 		 * products.
 		 */
-		if (((0f + asteroid.x - station.x) * (0f + asteroid_y - station.y)) == ((0f + asteroid_x - station.x)
-				* (0f + asteroid.y - station.y))
+		if (((asteroid.x - station.x) * (asteroid_y - station.y)) == ((asteroid_x - station.x)
+				* (asteroid.y - station.y))
 				&& Math.signum(asteroid.x - station.x) == Math.signum(asteroid_x - station.x)
 				&& Math.signum(asteroid.y - station.y) == Math.signum(asteroid_y - station.y)) {
 			return true;
